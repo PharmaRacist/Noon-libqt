2b1885d76f36d2335ce54cfa361d2a05c7d84cbe
Abdullah Muhamed <cdumb@proton.me>
2026-02-27 13:53:38 +0200

    clean build

--- build/src/services/.qt/noon_services_qml.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "noon_services")
-set(working_dir "/home/pharmaracist/Noon-libqt/src/services")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/pharmaracist/Noon-libqt/build/src/services/.qt/noon_services_qml.txt")
--- build/src/utils/qr/.qt/noon_utils_qr_qml.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "noon_utils_qr")
-set(working_dir "/home/pharmaracist/Noon-libqt/src/utils/qr")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/pharmaracist/Noon-libqt/build/src/utils/qr/.qt/noon_utils_qr_qml.txt")
